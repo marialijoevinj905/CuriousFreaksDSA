e4c6650d3c6c2d3572df5db96a1aac20aac7e06a
marialijoevinj905 <133891036+marialijoevinj905@users.noreply.github.com>
2025-03-29 13:26:28 +0530

    Update PrimetillX.java

--- Week_3/PrimetillX.java
+++ Week_3/PrimetillX.java
@@ -1,18 +1,15 @@
 public class PrimetillX{
-    private static boolean findPrime(int num){
-        for(int i=2;i<num;i++){
-		    if(num%i==0)
-		        return false;
-		}    
-		return true;
-    }
 	public static void main(String[] args) {
-		int n=5;
-		boolean res=findPrime(n);
-		if(res){
-		    System.out.println(n+" is Prime number");
-		}else{
-		    System.out.println(n+" is not a Prime number");
+		int n=11;
+		for(int x=2;x<=n;x++){
+		    int flag=0;
+		    for(int i=2;i<x;i++){
+		        if(x%i==0){
+		            flag=1;
+		        }
+		    }
+		    if(flag==0)
+		        System.out.print(x+" ");
 		}
 	}
 }
